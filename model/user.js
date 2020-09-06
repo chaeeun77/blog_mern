13216eb596483d7a780cd18a98afd19163c609ee
chaeeun <chaeeun77@naver.com>
2020-09-06 20:41:09 +0900

    user model setting

--- model/user.js
+++ model/user.js
@@ -1,6 +1,34 @@
 const mongoose = require('mongoose')
 
-const userSchema = new mongoose.Schema()
+const userSchema = new mongoose.Schema(
+    {
+        name: {
+            type: String,
+            required: true
+        },
+        email: {
+            type: String,
+            required: true,
+            unique: true
+        },
+        password: {
+            type: String,
+            required: true
+        },
+        avatar: {
+            type: String
+        }, //프로필 이미지
+        role: {
+            type: String,
+            default: "user" //회원등급별, 관리자인지 user인지
+        },
+        resetPasswordLink: ""
+    },
+    {
+        timestamps: true //업데이트 날짜, 생성날짜
+    }
+
+)
 
 module.exports = mongoose.model("user", userSchema)
 
